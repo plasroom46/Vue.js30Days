ad496141a41c60b72917f108c99a956573e78ab5
家暐 莊 <amos6572@hotmail.com>
2021-05-10 15:20:41 +0800

    Day19 Vuex -> store ->state, mutations,  actions

---
+++ Day19/iron-demo/src/store/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    numbers: [1, 3, 5, 7, 9]
+  },
+  mutations: {
+    ADD_NUMBER(state, payload) {
+      state.numbers.push(payload)
+    }
+  },
+  actions: {
+    // 上下 2 種寫法相同
+    // addNumber({commit},number) {
+    //   commit("ADD_NUMBER", number)
+    // }
+    addNumber(context, number) {
+      context.commit("ADD_NUMBER", number)
+    }
+
+  },
+  modules: {}
+})

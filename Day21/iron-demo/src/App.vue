693f3710ec51c5acd76015aadb4775048ea2f460
家暐 莊 <amos6572@hotmail.com>
2021-05-10 15:34:58 +0800

    Day21 Router(上)

--- Day21/iron-demo/src/App.vue
+++ Day21/iron-demo/src/App.vue
@@ -1,20 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <!-- 顯示根路由對應的view -->
+      <router-link to="/">Home</router-link> |
+      <!-- 顯示/about對應的view -->
+      <router-link to="/about">About</router-link>
+    </div>
+    <!-- 基本使用，自動預設name屬性為default -->
+    <router-view/> 
+    <!-- 命名使用，此範例會強制渲染名為home的組件 -->
+    <!-- <router-view  name="home"/> -->
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -23,6 +20,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>

54848ae01f9ccc9520a9d5676c716e67848d751d
家暐 莊 <amos6572@hotmail.com>
2021-05-10 15:22:49 +0800

    Day20 Vuex -> store -> getters

--- Day19/iron-demo/src/store/index.js
+++ Day19/iron-demo/src/store/index.js
@@ -4,23 +4,31 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    numbers: [1, 3, 5, 7, 9]
-  },
-  mutations: {
-    ADD_NUMBER(state, payload) {
-      state.numbers.push(payload)
-    }
-  },
-  actions: {
-    // 上下 2 種寫法相同
-    // addNumber({commit},number) {
-    //   commit("ADD_NUMBER", number)
-    // }
-    addNumber(context, number) {
-      context.commit("ADD_NUMBER", number)
-    }
+      state: {
+        numbers: [1, 3, 5, 7, 9]
+      },
+      mutations: {
+        ADD_NUMBER(state, payload) {
+          state.numbers.push(payload)
+        }
+      },
+      actions: {
+        // 上下 2 種寫法相同
+        // addNumber({commit},number) {
+        //   commit("ADD_NUMBER", number)
+        // }
+        addNumber(context, number) {
+          context.commit("ADD_NUMBER", number)
+        }
 
-  },
-  modules: {}
-})
+      },
+      modules: {},
+      // 上下 2 種寫法相同
+      // getters: {
+      //   sortedNumbers(state) {
+      //     return state.numbers.sort((a, b) => a - b)
+      //   },
+      // },
+      getters: {
+        sortedNumbers: (state) => state.numbers.sort((a, b) => a - b)
+      })

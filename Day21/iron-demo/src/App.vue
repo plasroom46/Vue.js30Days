74eccc1ff015f650ecbcc84abb6249c0fce625df
家暐 莊 <amos6572@hotmail.com>
2021-05-10 16:03:49 +0800

    Day22 Router(下) "redirect的結果與想像不同: 沒有About.vue的內容"

--- Day21/iron-demo/src/App.vue
+++ Day21/iron-demo/src/App.vue
@@ -4,10 +4,20 @@
       <!-- 顯示根路由對應的view -->
       <router-link to="/">Home</router-link> |
       <!-- 顯示/about對應的view -->
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link
+        :to="{
+          name: 'User',
+          params: {
+            id: 1,
+          },
+        }"
+      >
+        User1
+      </router-link>
     </div>
     <!-- 基本使用，自動預設name屬性為default -->
-    <router-view/> 
+    <router-view />
     <!-- 命名使用，此範例會強制渲染名為home的組件 -->
     <!-- <router-view  name="home"/> -->
   </div>
--- Day21/iron-demo/src/router/index.js
+++ Day21/iron-demo/src/router/index.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import User from '../views/User.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
     path: '/',
     name: 'Home',
     component: Home
@@ -16,12 +17,30 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: () => import( /* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  // Params
+  {
+    path: '/users/:id?',
+    name: 'User',
+    component: User,
+  },
+  {
+    path: '/',
+    name: 'Home',
+    alias: ['home', 'homepage'], // 別名(alias)
+    component: Home,
+    // 上下 3 種寫法相同
+    // redirect: '/about' //重新導向(redirect)
+    redirect: About
+    // redirect: {
+    //   About
+    // }
+  },
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash', // 建議使用 hash
   base: process.env.BASE_URL,
   routes
 })

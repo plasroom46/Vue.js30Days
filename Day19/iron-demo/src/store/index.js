e0ad376932b52b8059594559f64362c09b1816da
家暐 莊 <amos6572@hotmail.com>
2021-05-25 09:23:41 +0800

    Day19 vuex：mutations 和 actions 參數補充說明

--- Day19/iron-demo/src/store/index.js
+++ Day19/iron-demo/src/store/index.js
@@ -8,11 +8,16 @@ export default new Vuex.Store({
         numbers: [1, 3, 5, 7, 9]
       },
       mutations: {
+        // 1. state: 你可以自由取用或更動state的數值
+        // 2. payload: 從actions傳來的參數
         ADD_NUMBER(state, payload) {
           state.numbers.push(payload)
         }
       },
       actions: {
+        // 1. context 是一個物件，裡面可以讓你使用store中的commit、getter或是state屬性
+        // 2.想傳入mutation的參數，沒有要傳入則可省略
+        
         // 上下 2 種寫法相同
         // addNumber({commit},number) {
         //   commit("ADD_NUMBER", number)
